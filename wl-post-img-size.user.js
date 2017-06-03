1c9eef478c6118ba92bf154d45c3134394609210
Sai Kurogetsu <kurogetsusai@gmail.com>
2017-06-03 07:04:23 +0000

    add resizing preview posts' images

--- wl-post-img-size.user.js
+++ wl-post-img-size.user.js
@@ -2,7 +2,7 @@
 // @name        wl-post-img-size
 // @namespace   wl-post-img-size
 // @description Sets maximum image width in WL posts to 100% of the post width.
-// @version     0.1.2
+// @version     0.2.0
 // @include     http://www.warlight.net/*
 // @include     https://www.warlight.net/*
 // @grant       none
@@ -11,5 +11,20 @@
 
 'use strict';
 
-[...document.querySelectorAll('div[id^=PostForDisplay_] > img')]
-	.forEach(img => img.style.maxWidth = '100%');
+/** Limits maximum size of all images.
+ * @returns {void} */
+function resizeImages() {
+	[...document.querySelectorAll('div[id^=PostForDisplay_] > img')]
+		.forEach(img => img.style.maxWidth = '100%');
+}
+
+/** Binds a function to all preview buttons.
+ * @param {function} onClick function to be fired on all preview buttons' click event
+ * @returns {void} */
+function bindPreviewButtons(onClick) {
+	[...document.querySelectorAll('input[id^=PreviewBtn_]')]
+		.forEach(button => button.addEventListener('click', onClick));
+}
+
+bindPreviewButtons(resizeImages);
+resizeImages();
